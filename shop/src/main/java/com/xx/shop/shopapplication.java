b80cdd0147ffc309ca46f7a930b034c15109ed5a
NoWaring <786337054@qq.com>
2018-03-15 15:51:57 +0800

    完成项目的所有自动生成

--- shop/src/main/java/com/xx/shop/shopapplication.java
+++ shop/src/main/java/com/xx/shop/shopapplication.java
@@ -1,4 +1,11 @@
 package com.xx.shop;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class shopapplication {
+    public static void main(String [] args)
+    {
+        SpringApplication.run(shopapplication.class, args);
+    }
 }
