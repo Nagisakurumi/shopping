f51a0353e2daf60c3d1ee0a77408e4f59015c364
NoWaring <786337054@qq.com>
2018-03-27 17:31:50 +0800

    添加前台页面

--- shop/src/main/java/com/xx/shop/service/Impl/OrderServiceImpl.java
+++ shop/src/main/java/com/xx/shop/service/Impl/OrderServiceImpl.java
@@ -1,7 +1,42 @@
 package com.xx.shop.service.Impl;
 
+import com.xx.shop.ResultModel.ResultMap;
+import com.xx.shop.dao.OrderMapper;
+import com.xx.shop.entity.Order;
+import com.xx.shop.entity.OrderDetail;
 import com.xx.shop.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+@Service
 public class OrderServiceImpl implements OrderService{
 
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Override
+    public ResultMap addOneOrder() {
+        return null;
+    }
+
+    @Override
+    public ResultMap deleteOrder(long orderId) {
+        return null;
+    }
+
+    @Override
+    public ResultMap completeOrder(long orderId) {
+        return null;
+    }
+
+    @Override
+    public Order queryOrderByOrderId(long orderId) {
+        return orderMapper.selectByPrimaryKey(orderId);
+    }
+
+    @Override
+    public List<OrderDetail> queryOrderByUserId(long userId) {
+        return null;
+    }
 }
---
+++ shop/src/main/webapp/js/head.js
@@ -0,0 +1,8 @@
+function tologion() { 
+    window.location.href = "login.html";
+ }
+
+ function returntopage(param) {
+    window.history.back(-1);
+}
+
